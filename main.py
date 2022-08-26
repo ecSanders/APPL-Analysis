0c449a847baceb7e1fadc62dd95de13d83e9fc38
ecSanders <ecsander0@gmail.com>
2022-08-26 14:59:40 -0600

    Added altair chart

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Getting Data Frame
+#%% Getting Data Frame
 df = pd.read_csv('HistoricalPrices.csv')
 # print(df)
 
-#Getting x and y 
+#%% Getting x and y 
 date = df.loc[:, "Date"].tolist()
-closing = (df.iloc[:, 4]).toList()
+closing = (df.iloc[:, 4]).tolist()
 
-
+#%% plot
 plt.plot(date, closing)
 plt.title("Closing Price vs Date")
 plt.xlabel("Date")
 plt.ylabel("Price")
 plt.show()
+# %%
+#!pip install altair
+import altair as alt
+alt.Chart(df, title='Price Over Time').mark_line().encode(
+    alt.X('Date:T'),
+    alt.Y(' Open:Q')
+)
+
+# %%
